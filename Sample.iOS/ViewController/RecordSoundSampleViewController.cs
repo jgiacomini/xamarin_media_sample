43ff2638052f7e939d73c0146201df159f7d0935
jgiacomini <ismac@ismacs-Mac.local>
2017-05-14 16:16:11 +0200

    Changement d’ordre des méthodes

--- Sample.iOS/ViewController/RecordSoundSampleViewController.cs
+++ Sample.iOS/ViewController/RecordSoundSampleViewController.cs
@@ -54,7 +54,7 @@ namespace Sample.iOS
             _playSoundButton = UIButton.FromType(UIButtonType.System);
             _playSoundButton.Frame = new CGRect(10, 210, View.Bounds.Width - 20, 40);
             _playSoundButton.SetTitle("Ecouter l'enregistrement", UIControlState.Normal);
-			_playSoundButton.Enabled = false;
+            _playSoundButton.Enabled = false;
 
             _recordButton.TouchUpInside += _recordButton_TouchUpInside;
 
@@ -68,73 +68,33 @@ namespace Sample.iOS
                                  _playSoundButton);
         }
 
-        private void _stopRecordButton_TouchUpInside(object sender, EventArgs arg)
-        {
-			_recorder.Stop();
-			_recorder.Dispose();
-			_recorder = null;
-
-			_stateOfRecordingLabel.Text = string.Format("Durée de l'enregistrement : {0:hh\\:mm\\:ss}", this._stopwatch.Elapsed);
-			_stopwatch.Stop();
-			_recordButton.Enabled = true;
-			_stopRecordButton.Enabled = false;
-			_playSoundButton.Enabled = true;   
-        }
-
-        private async void _playSoundButton_TouchUpInside(object sender, EventArgs arg)
-        {
-			_playSoundButton.Enabled = false;
-
-			try
-			{
-				Console.WriteLine("Ecoute de l'enregistrement :" + this._audioFilePath.ToString());
-                _stateOfRecordingLabel.Text = "Ecoute de l'enregistrement";
-
-                AudioSession.Category = AudioSessionCategory.AmbientSound;
-				AudioSession.SetActive(true);
-
-				AudioManager.Instance.LoadFromUrl(this._audioFilePath.ToString());
-				AudioManager.Instance.NumberOfLoops = 0;
-				AudioManager.Instance.Volume = 1;
-				await AudioManager.Instance.PlayAsync();
-				_playSoundButton.Enabled = true;
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine("There was a problem playing back audio: ");
-				Console.WriteLine(ex.Message);
-				_playSoundButton.Enabled = true;
-			}   
-        }
-
-
         private void _recordButton_TouchUpInside(object sender, EventArgs e)
         {
-			_stateOfRecordingLabel.Text = "Début de l'enregistrement";
+            _stateOfRecordingLabel.Text = "Début de l'enregistrement";
 
-			if (!PrepareAudioRecording())
-			{
-				_stateOfRecordingLabel.Text = "Impossible de lancer l'enregistrement";
+            if (!PrepareAudioRecording())
+            {
+                _stateOfRecordingLabel.Text = "Impossible de lancer l'enregistrement";
 
-				return;
-			}
+                return;
+            }
 
-			if (!_recorder.Record())
-			{
-				_stateOfRecordingLabel.Text = "Impossible de lancer l'enregistrement";
+            if (!_recorder.Record())
+            {
+                _stateOfRecordingLabel.Text = "Impossible de lancer l'enregistrement";
 
-				return;
-			}
+                return;
+            }
 
-			_stopwatch = new Stopwatch();
-			_stopwatch.Start();
-			_stateOfRecordingLabel.Text = "Enregistrement";
-			_recordButton.Enabled = false;
-			_stopRecordButton.Enabled = true;
-			_playSoundButton.Enabled = false;
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+            _stateOfRecordingLabel.Text = "Enregistrement";
+            _recordButton.Enabled = false;
+            _stopRecordButton.Enabled = true;
+            _playSoundButton.Enabled = false;
         }
 
-		private bool PrepareAudioRecording()
+        private bool PrepareAudioRecording()
         {
             var audioSession = AVAudioSession.SharedInstance();
             var err = audioSession.SetCategory(AVAudioSessionCategory.Record);
@@ -153,23 +113,21 @@ namespace Sample.iOS
             string fileName = string.Format("MonEnregistrement_{0}.aac", DateTime.Now.ToString("yyyyMMddHHmmss"));
             string fileFullPath = Path.Combine(Path.GetTempPath(), fileName);
 
-			// Convertit le fileFullPath en NSUrl
-			_audioFilePath = NSUrl.FromFilename(fileFullPath);
+            // Convertit le fileFullPath en NSUrl
+            _audioFilePath = NSUrl.FromFilename(fileFullPath);
                        
             // Crée le profil d'enregistrement
             var audioSettings = new AudioSettings();
-            //audioSettings.SampleRate = 44100.0f;
             audioSettings.Format = AudioFormatType.MPEG4AAC;
-            //audioSettings.NumberChannels = 1;
             audioSettings.AudioQuality = AVAudioQuality.Max;
 
-			// Création de l'enregistreur
-			NSError error = null;
+            // Création du dictaphone
+            NSError error = null;
 
             _recorder = AVAudioRecorder.Create(_audioFilePath, audioSettings, out error);
             if (_recorder == null || error != null)
             {
-                Console.WriteLine(string.Format("Erreur lors de la création de l'enregistreur : {0}",error));
+                Console.WriteLine(string.Format("Erreur lors de la création du dictaphone : {0}",error));
                 return false;
             }
 
@@ -183,6 +141,46 @@ namespace Sample.iOS
             }
 
             return true;
+        }
+
+        private void _stopRecordButton_TouchUpInside(object sender, EventArgs arg)
+        {
+            _recorder.Stop();
+            _recorder.Dispose();
+            _recorder = null;
+
+            _stateOfRecordingLabel.Text = string.Format("Durée de l'enregistrement : {0:hh\\:mm\\:ss}", this._stopwatch.Elapsed);
+            _stopwatch.Stop();
+            _recordButton.Enabled = true;
+            _stopRecordButton.Enabled = false;
+            _playSoundButton.Enabled = true;
+        }
+
+
+        private async void _playSoundButton_TouchUpInside(object sender, EventArgs arg)
+        {
+            _playSoundButton.Enabled = false;
+
+            try
+            {
+                Console.WriteLine("Ecoute de l'enregistrement :" + this._audioFilePath.ToString());
+                _stateOfRecordingLabel.Text = "Ecoute de l'enregistrement";
+
+                AudioSession.Category = AudioSessionCategory.AmbientSound;
+                AudioSession.SetActive(true);
+
+                AudioManager.Instance.LoadFromUrl(this._audioFilePath.ToString());
+                AudioManager.Instance.NumberOfLoops = 0;
+                AudioManager.Instance.Volume = 1;
+                await AudioManager.Instance.PlayAsync();
+                _playSoundButton.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Impossible de jouer l'enregistrement audio");
+                Console.WriteLine(ex.Message);
+                _playSoundButton.Enabled = true;
+            }
         }
     }
 }
