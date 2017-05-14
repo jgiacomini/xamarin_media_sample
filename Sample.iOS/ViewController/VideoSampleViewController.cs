777eb7f2373206204a55ac0d540138479ea7fa81
jgiacomini <ismac@ismacs-Mac.local>
2017-05-14 23:26:17 +0200

    Exemple vidéo

--- Sample.iOS/ViewController/VideoSampleViewController.cs
+++ Sample.iOS/ViewController/VideoSampleViewController.cs
@@ -1,15 +1,38 @@
-﻿using System;
+﻿using AVFoundation;
+using AVKit;
+using Foundation;
 using UIKit;
 namespace Sample.iOS
 {
-	public class VideoSampleViewController : UIViewController
+	public class VideoSampleViewController : AVPlayerViewController
 	{
+	
 		public VideoSampleViewController()
 		{
-			Title = "Video sample";
+			Title = "Lecteur vidéo";
 
 			View.BackgroundColor = UIColor.White;
 			this.EdgesForExtendedLayout = UIRectEdge.None;
-		}	
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+            // Création de l'url de la vidéo
+            NSUrl videoURL = NSUrl.FromFilename("video.m4v");
+
+            // Chargement du lecteur
+            Player = new AVPlayer(videoURL);
+            ShowsPlaybackControls = true;
+
+			Player.Play();
+		}
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+            Player.Pause();
+        }
 	}
 }
